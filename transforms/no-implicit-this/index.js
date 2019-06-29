2c05bce1f5a9da3f9940392b804a84f9c9bd397e
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-06-29 11:33:28 -0400

    Tests now fail appropriately

--- transforms/no-implicit-this/index.js
+++ transforms/no-implicit-this/index.js
@@ -1,17 +1,23 @@
 const { getParser } = require('codemod-cli').jscodeshift;
+const { parse: parseHbs, print: printHbs } = require('ember-template-recast');
+const fs = require('fs');
 // const { getOptions } = require('codemod-cli');
 
 module.exports = function transformer(file, api) {
   const j = getParser(api);
   // const options = getOptions();
 
-  return j(file.source)
-    .find(j.Identifier)
-    .forEach(path => {
-      path.node.name = path.node.name
-        .split('')
-        .reverse()
-        .join('');
-    })
-    .toSource();
+  const ast = parseHbs(file.source);
+
+  return printHbs(ast);
+  
+  // return j(file.source)
+    // .find(j.Identifier)
+    // .forEach(path => {
+    //   path.node.name = path.node.name
+    //     .split('')
+    //     .reverse()
+    //     .join('');
+    // })
+    // .toSource();
 };
