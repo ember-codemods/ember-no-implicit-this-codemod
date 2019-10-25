e3e4c1c08ba29feee99bf3468ce2cd13b9670567
Ryan Mark <rgmark@gmail.com>
2019-10-25 14:33:38 -0700

    update telemetry helpers tp (#38)

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
-const { gatherTelemetryForUrl } = require('ember-codemods-telemetry-helpers');
+const { gatherTelemetryForUrl, analyzeEmberObject } = require('ember-codemods-telemetry-helpers');
 const appLocation = process.argv[2];
 const args = process.argv.slice(3);
 
 (async () => {
-  await gatherTelemetryForUrl(appLocation);
+  await gatherTelemetryForUrl(appLocation, analyzeEmberObject);
 
   require('codemod-cli').runTransform(__dirname, 'no-implicit-this', args, 'hbs');
 })();
