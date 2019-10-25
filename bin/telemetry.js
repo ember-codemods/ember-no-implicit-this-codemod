17e5edbcbe9ebd8f392959899c2806a45469f885
Ryan Mark <rgmark@gmail.com>
2019-10-25 14:25:25 -0700

    update telemetry helpers tp

--- bin/telemetry.js
+++ bin/telemetry.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-const { gatherTelemetryForUrl } = require('ember-codemods-telemetry-helpers');
+const { gatherTelemetryForUrl, analyzeEmberObject } = require('ember-codemods-telemetry-helpers');
 
-gatherTelemetryForUrl(process.argv[2]);
+gatherTelemetryForUrl(process.argv[2], analyzeEmberObject);
