ed5bcc7c64cdf75fbf4fe7526772ebe13cd3fee6
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-16 13:13:49 +0100

    Add `debug` logging (#83)

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
-const { gatherTelemetryForUrl, analyzeEmberObject } = require('ember-codemods-telemetry-helpers');
+const debug = require('debug')('ember-no-implicit-this-codemod');
+const {
+  gatherTelemetryForUrl,
+  analyzeEmberObject,
+  getTelemetry,
+} = require('ember-codemods-telemetry-helpers');
 const appLocation = process.argv[2];
 const args = process.argv.slice(3);
 
 (async () => {
+  debug('Gathering telemetry data from %s ...', appLocation);
   await gatherTelemetryForUrl(appLocation, analyzeEmberObject);
+
+  let telemetry = getTelemetry();
+  debug('Gathered telemetry on %d modules', Object.keys(telemetry).length);
 
   require('codemod-cli').runTransform(__dirname, 'no-implicit-this', args, 'hbs');
 })();
