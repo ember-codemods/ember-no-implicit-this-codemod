444251109584e75b9b6ec1d36088a0d740bc700b
Krystan HuffMenne <krystan@tilde.io>
2023-05-31 10:32:10 -0700

    Fix tests

---
+++ test/fixtures/3.28/output/app/components/gherkyn.js
@@ -0,0 +1,5 @@
+import Component from '@glimmer/component';
+
+export default class GherkynComponent extends Component {
+  property = 'property-on-this';
+}
---
+++ test/fixtures/3.28/output/app/helpers/cucumbyr.js
@@ -0,0 +1,5 @@
+import { helper } from '@ember/component/helper';
+
+export default helper(function cucumbyr(positional /*, named*/) {
+  return positional;
+});
